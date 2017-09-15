a6682d467b6a1ec0f9c1d87941e8b9e954e696c5
Byzantine <morbyzantine@gmail.com>
2017-09-14 22:39:35 -0700

    Finish lab3 part A reliable

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,11 +3,17 @@ package raftkv
 import "labrpc"
 import "crypto/rand"
 import "math/big"
-
+import "log"
+import "sync"
 
 type Clerk struct {
+	mu      sync.Mutex
 	servers []*labrpc.ClientEnd
+
 	// You will have to modify this struct.
+	reqIdCounter int64 // This shall be a atomic var.
+	leaderIndex  int
+	me           int64 // UUID
 }
 
 func nrand() int64 {
@@ -20,6 +26,8 @@ func nrand() int64 {
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
+	ck.leaderIndex = -1
+	ck.me = nrand() // use a random number
 	// You'll have to add code here.
 	return ck
 }
@@ -39,6 +47,31 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
+	args := GetArgs{Key: key, ReqId: ck.reqIdCounter, ClientId: ck.me}
+	ck.reqIdCounter++
+	var reply GetReply
+	for {
+		callingIndex := int(nrand()) % len(ck.servers)
+		if ck.leaderIndex != -1 {
+			callingIndex = ck.leaderIndex
+		}
+		ok := ck.servers[callingIndex].Call("RaftKV.Get", &args, &reply)
+		if ok {
+			if reply.WrongLeader {
+				ck.leaderIndex = -1
+				// log.Println("Wrong leader")
+			} else {
+				// record leader
+				ck.leaderIndex = callingIndex
+			}
+			if reply.Err == OK {
+				// log.Println("Recv Get ok")
+				return reply.Value
+			}
+		} else {
+			log.Println("Network error")
+		}
+	}
 	return ""
 }
 
@@ -54,6 +87,36 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	args := PutAppendArgs{
+		Key:      key,
+		Value:    value,
+		Op:       op,
+		ReqId:    ck.reqIdCounter,
+		ClientId: ck.me}
+
+	ck.reqIdCounter++
+
+	var reply PutAppendReply
+	for {
+		callingIndex := int(nrand()) % len(ck.servers)
+		if ck.leaderIndex != -1 {
+			callingIndex = ck.leaderIndex
+		}
+		ok := ck.servers[callingIndex].Call("RaftKV.PutAppend", &args, &reply)
+		if ok {
+			if reply.WrongLeader {
+				ck.leaderIndex = -1
+			} else {
+				// record leader
+				ck.leaderIndex = callingIndex
+			}
+			if reply.Err == OK {
+				return
+			}
+		} else {
+			log.Println("Network error")
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -17,11 +17,28 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Key      string
+	Value    string // optional if it's Get
+	Op       string
+	ReqId    int64 // Used for notification of invocation response
+	ClientId int64
+}
+
+type Requests map[int64]chan Op
+
+func GetOrAdd(reqs *map[int64]Requests, clientId int64) *Requests {
+	req, ok := (*reqs)[clientId]
+	if ok {
+		return &req
+	} else {
+		(*reqs)[clientId] = make(map[int64]chan Op)
+		ret := (*reqs)[clientId]
+		return &ret
+	}
 }
 
 type RaftKV struct {
@@ -33,15 +50,83 @@ type RaftKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	kv map[string]string // The actual kv store
+
+	clientRequests map[int64]Requests // handler will wait for the Op to come
 }
 
+// Return true if current raft server is the leader.
+func (kv *RaftKV) TryWriteOp(op *Op, reqId int64, clientId int64) (bool, chan Op) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	// Start is no blocking.
+	_, _, isLeader := kv.rf.Start(*op)
+	var ret_chan chan Op
+	if isLeader {
+		requests := GetOrAdd(&kv.clientRequests, clientId)
+		(*requests)[reqId] = make(chan Op)
+
+		ret_chan = (*requests)[reqId]
+	}
+	return isLeader, ret_chan
+}
 
 func (kv *RaftKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	// kv.mu.Lock()
+	// defer kv.mu.Unlock()
+	// log.Println("Me: ", kv.me, " recv Get: ", args)
+
+	isLeader, req_chan :=
+		kv.TryWriteOp(
+			&Op{
+				Key:      args.Key,
+				Op:       "Get",
+				ReqId:    args.ReqId,
+				ClientId: args.ClientId},
+			args.ReqId,
+			args.ClientId)
+
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	op := <-req_chan
+
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	value, ok := kv.kv[op.Key]
+	if ok {
+		reply.Err = OK
+		reply.Value = value
+	} else {
+		reply.Err = ErrNoKey
+	}
+	reply.WrongLeader = false
 }
 
 func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	// log.Println("Me: ", kv.me, " recv PutAppend: ", args)
+	isLeader, req_chan :=
+		kv.TryWriteOp(
+			&Op{
+				Key:      args.Key,
+				Value:    args.Value,
+				Op:       args.Op,
+				ReqId:    args.ReqId,
+				ClientId: args.ClientId},
+			args.ReqId,
+			args.ClientId)
+
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	// must not hold a lock when waiting.
+	<-req_chan
+	reply.Err = OK
+	reply.WrongLeader = false
 }
 
 //
@@ -53,6 +138,31 @@ func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 func (kv *RaftKV) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
+}
+
+func (kv *RaftKV) Apply() {
+	// Read from the applyCh.
+	for {
+		select {
+		case msg := <-kv.applyCh:
+			// if there exists a command.ReqId in the map.
+			op := msg.Command.(Op)
+			kv.mu.Lock()
+			if op.Op == "Put" {
+				kv.kv[op.Key] = op.Value
+			}
+			if op.Op == "Append" {
+				kv.kv[op.Key] += op.Value
+			}
+			// do nothing when it's Get.
+			requests := GetOrAdd(&kv.clientRequests, op.ClientId)
+			request, ok := (*requests)[op.ReqId]
+			kv.mu.Unlock()
+			if ok {
+				request <- op
+			}
+		}
+	}
 }
 
 //
@@ -76,6 +186,8 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv := new(RaftKV)
 	kv.me = me
 	kv.maxraftstate = maxraftstate
+	kv.clientRequests = make(map[int64]Requests)
+	kv.kv = make(map[string]string)
 
 	// You may need initialization code here.
 
@@ -84,5 +196,6 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	// You may need initialization code here.
 
+	go kv.Apply()
 	return kv
 }
